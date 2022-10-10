f7174b777817254cf5f72ca738d2b63334437a6f
go140point6 <50885069+go140point6@users.noreply.github.com>
2022-10-10 15:05:46 -0700

    bot

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { Client } from "discord.js";
 import { IntentOptions } from "./config/IntentsOptions";
 
 (async () => {
-    const BOT = new Client({ intents: IntentOptions });
+    const client = new Client({ intents: IntentOptions });
 
-    await BOT.on("ready", () => console.log(client.user?.username, "is ready!"))
-    await BOT.login(process.env.BOT_TOKEN);
+    await client.on("ready", () => console.log(client.user?.username, "is ready!"))
+    await client.login(process.env.BOT_TOKEN);
 })();
